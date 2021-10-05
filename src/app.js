337afa4df73580094d0b37005cbb3b78437cee50
MagicHacker <zxmagichacker@gmail.com>
2021-10-05 22:45:22 +0800

    feat: 添加日志中间件

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@
 const express = require('express')
 const config = require('./config')
 const router = require('./router')
+const morgan = require('morgan')
 const app = express()
 
 // 解析请求体
 app.use(express.json())
 app.use(express.urlencoded())
+// 日志记录
+app.use(morgan('short'))
 // 托管静态资源
 app.use(express.static('./public'))
 // 挂载路由
