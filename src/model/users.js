89ca9de8992100e6c05ad94510b0efc3819de19b
MagicHacker <zxmagichacker@gmail.com>
2021-10-08 23:01:46 +0800

    feat: 构建用户表模型

--- src/model/users.js
+++ src/model/users.js
@@ -1,3 +1,35 @@
 /**
  * 用户模型
  */
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../utils/sequelize');
+
+const User = sequelize.define(
+	'user',
+	{
+		// 模型属性对应表的字段
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		signature: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+		avatar: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+	},
+	{
+		freezeTableName: true, // 强制表名称=模型名称，防止sequelize自动复数化表名
+	},
+);
+
+module.exports = {
+	User,
+};
