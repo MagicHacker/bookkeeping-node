ffc3aa070fb8f6c13dc9fd106709fc30757ca2ad
MagicHacker <zxmagichacker@gmail.com>
2021-10-10 18:12:27 +0800

    feat: 构建错误信息配置文件

---
+++ src/config/error.js
@@ -0,0 +1,31 @@
+/**
+ * 错误信息处理
+ */
+
+module.exports = {
+	success: {
+		code: 0,
+		msg: '',
+		status: 200,
+	},
+	error: {
+		code: 1,
+		msg: 'service error',
+		status: 500,
+	},
+	bad_request: {
+		code: 1,
+		msg: 'Bad Request',
+		status: 400,
+	},
+	noResult: {
+		code: 1,
+		msg: '404 no resource',
+		status: 404,
+	},
+	forbidden: {
+		code: 1,
+		msg: 'Request Forbidden',
+		status: 403,
+	},
+};
