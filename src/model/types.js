3e269e32f2a7404c054bd3dab2c4d664d62df5a9
MagicHacker <zxmagichacker@gmail.com>
2021-10-10 12:03:04 +0800

    feat:构建账单类型模型

--- src/model/types.js
+++ src/model/types.js
@@ -1,3 +1,23 @@
 /**
  * 账单类型模型
  */
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../utils/sequelize');
+
+const types = sequelize.define('type', {
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		comment: '标签名称',
+	},
+	type: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+		comment: '标签类型',
+	},
+});
+
+module.exports = {
+	types,
+};
