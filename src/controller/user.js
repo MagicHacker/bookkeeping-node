aace113abda18264dcc67248ca814c6f633a6ae6
MagicHacker <zxmagichacker@gmail.com>
2021-10-13 23:17:41 +0800

    feat: 添加用户查询，用户更新方法

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,24 +2,24 @@
  * 用户相关逻辑
  */
 const { Op } = require('sequelize');
-const { User } = require('../model/users');
+const { Users } = require('../model');
 // 用户注册controller
 const userRegister = async (username, password) => {
 	try {
-		const user = await User.create({
+		const user = await Users.create({
 			username,
 			password,
 		});
 		return user;
 	} catch (error) {
-		console.error('error msg:', error);
+		console.error('userRegister error msg:', error);
 	}
 };
 
 // 用户登录controller
 const userLogin = async (username, password) => {
 	try {
-		const user = User.findOne({
+		const user = await Users.findOne({
 			where: {
 				[Op.and]: [
 					{
@@ -33,11 +33,45 @@ const userLogin = async (username, password) => {
 		});
 		return user;
 	} catch (error) {
-		console.error('error msg:', error);
+		console.error('userLogin error msg:', error);
 	}
 };
-
+// 用户信息查询
+const userQuery = async username => {
+	try {
+		const user = await Users.findOne({
+			where: {
+				username,
+			},
+		});
+		return user;
+	} catch (error) {
+		console.error('userQuery error msg:', error);
+	}
+};
+// 用户信息更新
+const userUpdate = async ({ username, password, signature, avatar }) => {
+	try {
+		const user = await Users.update(
+			{
+				password,
+				signature,
+				avatar,
+			},
+			{
+				where: {
+					username,
+				},
+			},
+		);
+		return user;
+	} catch (error) {
+		console.error('userUpdate error msg:', error);
+	}
+};
 module.exports = {
 	userRegister,
 	userLogin,
+	userQuery,
+	userUpdate,
 };
