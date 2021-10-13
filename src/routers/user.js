6ce01744d29d164e5da2201dfe29c0aa37f93ac0
MagicHacker <zxmagichacker@gmail.com>
2021-10-13 23:18:02 +0800

    feat: 新增用户查询接口

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@
  */
 const express = require('express');
 const router = express.Router();
-const { userRegister, userLogin } = require('../controller/user');
+const { userRegister, userLogin, userQuery } = require('../controller/user');
 const { success, error, bad_request } = require('../config/error');
 
 // 用户注册
@@ -36,6 +36,24 @@ router.post('/login', (req, res) => {
 		const user = userLogin(username, password);
 		if (user) {
 			res.status(200).send(success);
+		} else {
+			res.status(500).send(error);
+		}
+	} else {
+		res.status(400).send(bad_request);
+	}
+});
+
+// 用户查询
+router.get('/queryUser', (req, res) => {
+	const {
+		params: { username },
+	} = req;
+	// 参数判空处理
+	if (username) {
+		const user = userQuery(username);
+		if (user) {
+			res.status(200).send({ ...success, data: { ...user } });
 		} else {
 			res.status(500).send(error);
 		}
