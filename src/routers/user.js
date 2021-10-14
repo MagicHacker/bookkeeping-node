2c99965a431761a0050843f16d2fb223a895bd69
MagicHacker <zxmagichacker@gmail.com>
2021-10-14 23:58:37 +0800

    feat: 添加用户更新接口，修改用户查询接口bug

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,11 +3,16 @@
  */
 const express = require('express');
 const router = express.Router();
-const { userRegister, userLogin, userQuery } = require('../controller/user');
+const {
+	userRegister,
+	userLogin,
+	userQuery,
+	userUpdate,
+} = require('../controller/user');
 const { success, error, bad_request } = require('../config/error');
 
 // 用户注册
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
 	const {
 		body: { username, password },
 	} = req;
@@ -15,7 +20,7 @@ router.post('/register', (req, res) => {
 	if (username && password) {
 		// 先查询数据是否已存在
 		// 注册逻辑
-		const user = userRegister(username, password);
+		const user = await userRegister(username, password);
 		if (user) {
 			res.status(200).send(success);
 		} else {
@@ -27,13 +32,13 @@ router.post('/register', (req, res) => {
 });
 
 // 用户登录
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 	const {
 		body: { username, password },
 	} = req;
 	// 判空处理
 	if (username && password) {
-		const user = userLogin(username, password);
+		const user = await userLogin(username, password);
 		if (user) {
 			res.status(200).send(success);
 		} else {
@@ -45,15 +50,33 @@ router.post('/login', (req, res) => {
 });
 
 // 用户查询
-router.get('/queryUser', (req, res) => {
+router.get('/queryUser', async (req, res) => {
 	const {
-		params: { username },
+		query: { username },
 	} = req;
 	// 参数判空处理
 	if (username) {
-		const user = userQuery(username);
+		const user = await userQuery(username);
 		if (user) {
 			res.status(200).send({ ...success, data: { ...user } });
+		} else {
+			res.status(500).send(error);
+		}
+	} else {
+		res.status(400).send(bad_request);
+	}
+});
+
+// 用户更新
+router.post('/updateUser', (req, res) => {
+	const {
+		body: { username, password, signature, avatar },
+	} = req;
+	// 参数判空处理
+	if (username) {
+		const user = userUpdate({ username, password, signature, avatar });
+		if (user) {
+			res.status(200).send({ ...success, msg: 'update success' });
 		} else {
 			res.status(500).send(error);
 		}
