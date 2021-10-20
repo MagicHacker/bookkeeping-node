d5b41691619838c32cc4ad24cbdad4e30ea2e79f
MagicHacker <zxmagichacker@gmail.com>
2021-10-20 22:54:19 +0800

    feat: 添加账单功能

--- src/controller/bill.js
+++ src/controller/bill.js
@@ -1,3 +1,12 @@
 /**
  * 账单相关逻辑
  */
+
+// 添加账单
+const billAdd = (...args) => {
+	console.warn('你的值', args);
+};
+
+module.exports = {
+	billAdd,
+};
--- src/routers/bill.js
+++ src/routers/bill.js
@@ -3,5 +3,31 @@
  */
 const express = require('express');
 const router = express.Router();
-
+const { bad_request, error, success } = require('../config/error');
+const { billAdd } = require('../controller/bill');
+// 添加账单
+router.post('/billAdd', async (req, res) => {
+	const {
+		body: { pay_type, amount, date, type_id, type_name, remark = '' },
+	} = req;
+	// 参数判空处理
+	if (pay_type || amount || date || type_id || type_name) {
+		// 获取token
+		const bill = await billAdd(
+			pay_type,
+			amount,
+			date,
+			type_id,
+			type_name,
+			remark,
+		);
+		if (bill) {
+			res.status(200).send({ ...success, ...bill });
+		} else {
+			res.status(500).send(error);
+		}
+	} else {
+		res.status(400).send(bad_request);
+	}
+});
 module.exports = router;
